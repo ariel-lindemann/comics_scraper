aad04e3c45aeeba1be6a73a851aa032e54cd619e
Ariel Lindemann <ga92xan@mytum.de>
2022-02-18 02:51:42 +0100

    function get multiple issues

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ from PIL import Image
 BASE_URL = 'http://readallcomics.com'
 COMICS_DIR = ''
 
-images_dir = 'imgs'
 
 def get_data(url):
     r = requests.get(url)
     return r.text
 
 
-def get_imgs(url, skip = 2):
+def get_imgs(url, skip=2):
     print('getting images...')
     htmldata = get_data(url)
     soup = BeautifulSoup(htmldata, 'html.parser')
@@ -23,15 +22,15 @@ def get_imgs(url, skip = 2):
 
     for i in images_soup:
         print('.', end='')
-        x+=1
+        x += 1
         img_url = i['src']
         img = Image.open(requests.get(img_url, stream=True).raw)
-        if(x>=skip):
+        if(x >= skip):
             img = img.convert('RGB')
             images_pil.append(img)
             vertical = (img.size[0] < img.size[1])
             if not vertical:
-                x+=1
+                x += 1
 
     return images_pil
 
@@ -39,21 +38,27 @@ def get_imgs(url, skip = 2):
 def get_issue(issue_url, skip):
     print(f'getting issue: {issue_url}...')
     url = f'{BASE_URL}/{issue_url}'
-    
+
     images = get_imgs(url, skip=skip)
 
     cover = images[0]
     rest = images[1:]
     pdf_title = issue_url.replace('-', ' ')
 
-    cover.save(f'{COMICS_DIR}{pdf_title}.pdf', 'PDF', save_all=True, append_images=rest)
+    cover.save(f'{COMICS_DIR}{pdf_title}.pdf', 'PDF',
+               save_all=True, append_images=rest)
 
-    # make_pdf(images, 'comic.pdf')
 
-if __name__=='__main__':
+def get_multiple_issues(search_str, last, first=1, skip=2):
+
+    for i in range(first, last+1):
+        s = search_str.format(nr=i)
+        get_issue(s, skip)
+
+
+if __name__ == '__main__':
     skip = 2
     i = 7
 
-
-    search_str = f'aquatlantic-full'
-    get_issue(search_str, skip)
+    search_str = 'x-lives-of-wolverine-{nr}-2022'
+    get_multiple_issues(search_str, 3)
