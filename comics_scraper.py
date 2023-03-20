5cd1a15cc76b3ca2d8b0e001d0228bbeee92a1f1
Ariel Lindemann <ga92xan@mytum.de>
2023-03-20 14:59:42 +0100

    implemented series search cli interaction

--- comics_scraper.py
+++ comics_scraper.py
@@ -1,8 +1,26 @@
 from comics_scraper.main import main
 from comics_scraper.comics import Series
+from comics_scraper.downloader import find_links
+
+def cli_interaction():
+    print('Hello, welcome to comics scraper!\nWhich series are you interested in?')
+    series_name = input()
+    links_dict = find_links(series_name)
+
+    if not links_dict:
+        print(f'Sorry, we couldn\'t find any series named \"{series_name}\"')
+
+    else: print('Select one of the following:')
+    for i, e in enumerate(links_dict.keys()):
+        print(f'    {i}.\t {e}')
+
+
 
 if __name__ == '__main__':
 
-    series = Series('new-xmen-v1-{issue}')
-    series.add_multiple_issues(range(133, 135))
-    main(series)
+    cli_interaction()
+
+    # TODO issue_page
+    # TODO CLI
+
+
