1f87feb88648415d4fd65ffcedd2eb37609dc193
ariel-lindemann <ariel.lindemann@gmail.com>
2023-03-20 21:36:36 +0100

    implemented downloading single issue from cli

--- comics_scraper.py
+++ comics_scraper.py
@@ -1,13 +1,14 @@
 import cli
+from comics_scraper import download_multiple_issues
+
 
 if __name__ == '__main__':
 
     selected_issues = cli.cli_interaction()
     print(selected_issues)
 
-
+    download_multiple_issues(selected_issues)
+    print('Finished downloading')
 
     # TODO issue_page
     # TODO CLI
-
-
--- comics_scraper/__init__.py
+++ comics_scraper/__init__.py
@@ -1,3 +1,4 @@
 from .link_finder import search_series, filter_links, find_links_on_page
+from .downloader import download_multiple_issues
 
 __version__ = '0.1.1'
