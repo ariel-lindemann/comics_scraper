02d64c10c6cec0c7f3e0806fb16a205f10ae13b4
ariel-lindemann <ariel.lindemann@gmail.com>
2023-04-13 13:01:57 +0200

    removed unneeded file

--- comics_scraper/main.py
+++
@@ -1,8 +0,0 @@
-from comics_scraper.downloader import download_issue, download_multiple_issues, find_links
-from comics_scraper.comics import Issue, Series
-
-
-def main(series: Series):
-
-    issues = series.get_all_issues()
-    download_multiple_issues(issues)
