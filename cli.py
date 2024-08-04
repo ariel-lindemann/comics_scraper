294f60fe254461ff57b332f74480173f10515510
Ariel Lindemann <ga92xan@mytum.de>
2024-08-04 12:26:23 +0200

    added interactive cli

--- cli.py
+++ cli.py
@@ -1,5 +1,16 @@
+import string
 from typing import Optional
 from comics_scraper import search_series, filter_links, find_links_on_page
+
+from prompt_toolkit import PromptSession
+from prompt_toolkit.application import Application
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.layout.containers import HSplit, Window
+from prompt_toolkit.layout.controls import FormattedTextControl
+from prompt_toolkit.layout.layout import Layout
+from prompt_toolkit.styles import Style
+from prompt_toolkit.widgets import Box, Frame
+
 
 RETURN_COMMAND = 'END'
 
@@ -23,7 +34,7 @@ def cli_interaction() -> Optional[dict[str, str]]:
         else:
             _list_links(links_dict, 'Select one of the following')
             try:
-                series_name, series_url = _selection_modal(links_dict)
+                series_name, series_url = SelectionModal(links_dict).run()
             except TypeError:
                 break
             print(f'You selected \"{series_name}\"')
@@ -50,7 +61,7 @@ def issue_selection(series_url: str, selected: dict[str, str]) -> dict[str, str]
     while True:
         _list_links(issues_links, 'Select the issue you are interested in')
         try:
-            selected_name, selected_link = _selection_modal(issues_links)
+            selected_name, selected_link = SelectionModal(issues_links).run()
         except TypeError:
             break
         print(f'You selected the following issue: {selected_name}')
@@ -82,7 +93,7 @@ def _selection_modal(links_dict: dict[str, str]) -> Optional[tuple[str, str]]:
             (k, v) = links_dict.popitem()
             return k, v
         print('Narrow the list down by typing more words or type \'#\' before a number to select an entry directly.')
-        selection = input()
+        selection = input('Search: ').strip()
         if not selection:
             continue
         if selection == RETURN_COMMAND:
@@ -99,6 +110,97 @@ def _selection_modal(links_dict: dict[str, str]) -> Optional[tuple[str, str]]:
             links_dict = unfiltered_links
 
         _list_links(links_dict, 'Select one of the following')
+
+
+class SelectionModal:
+    def __init__(self, links_dict: dict[str, str]):
+        self.links_dict = links_dict
+        self.unfiltered_links = links_dict.copy()
+        self.filtered_links = links_dict
+        self.query = ""
+
+        self.bindings = KeyBindings()
+        self.bindings.add('c-c')(self.exit)
+        self.bindings.add('c-q')(self.exit)
+        self.bindings.add('enter')(self.select)
+        self.bindings.add('backspace')(self.backspace)
+
+        for key in string.printable:
+            self.bindings.add(key)(self.add_char(key))
+
+        self.layout = Layout(HSplit([
+            Box(Window(FormattedTextControl(self.get_display_text), height=1)),
+            Box(Window(FormattedTextControl(self.get_hits_text), height=1)),
+            Box(Window(FormattedTextControl(self.get_results_text), height=20, always_hide_cursor=True))
+        ]))
+
+        self.application = Application(
+            layout=self.layout,
+            key_bindings=self.bindings,
+            full_screen=True,
+            mouse_support=True,
+            style=Style.from_dict({
+                'window.border': 'cyan',
+                'shadow': 'gray',
+            }),
+        )
+
+    def get_display_text(self):
+        return [("class:text", f"Search: {self.query}")]
+
+    def get_hits_text(self):
+        return [("class:text", f"Your selection mathes {len(self.filtered_links)} items:")]
+
+    def get_results_text(self):
+        if not self.filtered_links:
+            return [("class:text", "No entries match your input. Please try again.")]
+
+        return [("class:text", f"{i}: {k}\n") for i, k in enumerate(self.filtered_links.keys())]
+
+    def add_char(self, char):
+        def handler(event):
+            self.query += char
+            self.filtered_links = filter_links(self.query, self.unfiltered_links)
+            self.application.layout = Layout(HSplit([
+                Box(Window(FormattedTextControl(self.get_display_text), height=1)),
+                Box(Window(FormattedTextControl(self.get_hits_text), height=1)),
+                Box(Window(FormattedTextControl(self.get_results_text), height=10, always_hide_cursor=True))
+            ]))
+        return handler
+
+    def backspace(self, event):
+        self.query = self.query[:-1]
+        self.filtered_links = filter_links(self.query, self.unfiltered_links)
+        self.application.layout = Layout(HSplit([
+            Box(Window(FormattedTextControl(self.get_display_text), height=1)),
+            Box(Window(FormattedTextControl(self.get_hits_text), height=1)),
+            Box(Window(FormattedTextControl(self.get_results_text), height=10, always_hide_cursor=True))
+        ]))
+
+    def select(self, event):
+        if len(self.filtered_links) == 1:
+            self.selected_item = list(self.filtered_links.items())[0]
+            self.application.exit()
+        elif self.query.startswith('#'):
+            try:
+                index = int(self.query[1:])
+                if 0 <= index < len(self.filtered_links):
+                    self.selected_item = list(self.filtered_links.items())[index]
+                    self.application.exit()
+                else:
+                    self.query = "Invalid index. Try again."
+                    self.filtered_links = self.unfiltered_links
+            except ValueError:
+                self.query = "Invalid format. Try again."
+                self.filtered_links = self.unfiltered_links
+
+    def exit(self, event):
+        self.selected_item = None
+        self.application.exit()
+
+    def run(self):
+        self.application.run()
+        return self.selected_item
 
 
 def _direct_selection(links_dict: dict[str, str], selection: str) -> Optional[tuple[str, str]]:
